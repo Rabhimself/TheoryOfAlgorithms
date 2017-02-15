257491e8d672a8516f767a96a0a99f46d27f82b3
Robert Bryant <Robert Bryant>
2017-02-15 15:33:36 +0000

    applying some operations from one list to another list

--- scripts.rkt
+++ scripts.rkt
@@ -8,7 +8,16 @@
 ;defining test numbers
 (define a 5)
 (define b 10)
+(define c 15)
+;defining a list for later
+(define lst (list a b c))
 
-;
+;small check if the operation yields a match to the target
 (equal? ((car ops) a b) tar )
- 
+
+;more building blocks, grabs the first operation in the ops list, and applies it to the
+;first item in the list argument with the second item in the list argument
+(define (doMath lst)
+ ((car ops) (car lst) (car (cdr lst))))
+
+(doMath lst)

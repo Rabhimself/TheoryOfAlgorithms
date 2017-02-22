c26f4e3820e4d418c78ef87d431b0dc3f11e55be
Robert Bryant <rabhimself86@gmail.com>
2017-02-22 13:59:59 +0000

    Found permutations in racket docs. Would come in handy for later, but it seems a bit like cheating. Might end up having to make my own implementation of it.

--- scripts.rkt
+++ scripts.rkt
@@ -24,6 +24,8 @@
   (if(null? (cdr lst))
      (car lst)
      (op (car lst)  (applyOpToList op (cdr lst)))))
+;if I use the permutations function this would be useless.
+;as i'd just end up essentially merging the ordered operations list with every permutation
 
 ;considering looping over all different orders of the numbers and printing them
 ;then maybe change around the operations as an overall algorithm
@@ -35,7 +37,19 @@
 ; just pushing to a temp list, plan on using temp to build the different combinations of operations/numbers
 (define (doLoop lst)
   (if ( = (length lst) 2)
-  (list* (car lst) (cons (cadr lst) temp))
+  (doLoopEnd (car lst) (cadr lst) temp)
   (list* (car lst) (doLoop (cdr lst)))))
 
-(doLoop lst)
+(define (doLoopEnd a b temp)
+  (list* a b temp))
+
+
+(define permus (permutations lst))
+;not sure if this is kosher, but leaving it for later just in case
+;it generates every single permutation of a lists elements w/o repeats
+;I still need to get every single permutation WITH repeats on the operations
+;then combine the two. Likely easier said than done...
+;Serious brute force stuff here
+
+
+
